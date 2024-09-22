1cdde8c4ea609b743fa59703b56efc08e6bf5e42
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-22 12:57:33 +0500

    skibidi comt

--- main.py
+++ main.py
@@ -15,3 +15,11 @@ if ( x is y ):
 y=30
 if( x is not y):
      print("x & y have different identity")
+a = 10
+b = -10
+print("a >>  1 =", a >> 1)
+print("b >>  1 =", b >> 1)
+a = 5
+b = -10
+print("a <<  1 =", a << 1)
+print("a <<  1 =", a << 1)
